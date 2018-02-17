a3e18e9275ade7ef32f60ee9fe87ce625c9135d3
Yamir121 <32955694+Yamir121@users.noreply.github.com>
2018-02-17 20:28:41 +0100

    QR reader Test

---
+++ QR-Code Test/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour
+{
+    QRReader reader = new QRReader();
+
+    private void Start()
+    {
+        reader.Create();
+        StartCoroutine(reader.Read());
+    }
+
+    private void OnGUI()
+    {
+        reader.Draw();
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "DECODED TEXT FROM QR: " + reader.text);
+    }
+}
---
+++ QR-Code Test/Assets/Scripts/QRReader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+
+public class QRReader
+{
+
+    private WebCamTexture camTexture;
+    private Rect screenRect;
+    public string text = "";
+
+    public QRReader()
+    {}
+
+    public void Create()
+    {
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        camTexture = new WebCamTexture
+        {
+            requestedHeight = Screen.height,
+            requestedWidth = Screen.width
+        };
+        if (camTexture != null)
+        {
+            camTexture.Play();
+        }
+    }
+
+    public void Draw()
+    {
+        GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
+    }
+
+    public IEnumerator Read()
+    {
+        IBarcodeReader barcodeReader = new BarcodeReader();
+        var result = barcodeReader.Decode(camTexture.GetPixels32(),camTexture.width, camTexture.height);
+        if (result.Text != null)
+        {
+            Debug.Log("Not quite");
+            text = result.Text;
+            yield return null;
+        }
+        Debug.Log("Madeit");
+        yield return new WaitForSeconds(2);
+    }
+}
+
+
