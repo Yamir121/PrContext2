98f2fc2bc27e158bfe0a6cc49e2702bdb7bdf260
nadinevdberg <nadine-vdberg@hotmail.com>
2018-03-02 10:39:22 +0100

    Prototype_v8
    Geupdate naar nieuwe unity versie

--- Pocket Pieces/Assets/Scripts/Managers/GameManager.cs
+++ Pocket Pieces/Assets/Scripts/Managers/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour {
     public static GameManager Instance { get { return _instance; } }
     private static GameManager _instance;
 
+    //helemaal aan het begin, zodra het spel start. 1x van begin tot eind
+    // als er al een instance van de game manager bestat, dan jezelf destroyen en anders jezelf aanmaken
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -30,9 +32,11 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    private void Start()
+    //start wordt 1x van begin tot eind uitgevoerd zodra het gameobject waar het script op zit wordt geinstantiate. 
+    private void Start() 
     {
         UIManager.Instance.CreateScreen(0);
+        UIManager.Instance.CreatePopUp(0, "Hello World");
     }
 
 }
