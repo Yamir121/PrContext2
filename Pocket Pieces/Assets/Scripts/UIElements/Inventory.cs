31645ab04f149b5aa5a05e2f31b2bfb692e7a188
nadinevdberg <nadine-vdberg@hotmail.com>
2018-03-02 15:06:38 +0100

    Prototype v8
    Updates

---
+++ Pocket Pieces/Assets/Scripts/UIElements/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : UIElement
+{
+
+    public override void Setup()
+    {
+
+    }
+}
---
+++ Pocket Pieces/Assets/Scripts/UIElements/Map.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : UIElement
+{
+
+    public override void Setup()
+    {
+
+    }
+}
